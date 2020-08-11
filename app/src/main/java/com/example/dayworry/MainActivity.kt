b461651e50ee779c85f905c9769c7e9aa938dbe9
inju2403 <inju2403@gmail.com>
2020-08-11 17:51:34 +0900

    1. add custom bottomBar, Fragments, addWorryActivity 2. AddWorryActivity에서 고민 작성 후 게시 버튼을 누르면 고민 목록 프래그먼트로 이동

--- app/src/main/java/com/example/dayworry/MainActivity.kt
+++ app/src/main/java/com/example/dayworry/MainActivity.kt
@@ -1,12 +1,127 @@
 package com.example.dayworry
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dayworry.counsel.CounselFragment
+import com.example.dayworry.mypage.MyPageFragment
+import com.example.dayworry.worry.WorryFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val counselFragment = CounselFragment()
+    private val myPageFragment = MyPageFragment()
+    private val worryFragment = WorryFragment()
+
+    private val FRAG_COUNSEL = 0
+    private val FRAG_WORRY = 1
+    private val FRAG_MYPAGE = 2
+
+    private val REQUEST_MAINACTIVITY_CODE = 100
+    private val RETURN_OK = 101
+
+    private var counselJudge = 1 //선택: 1, 미선택: 0
+    private var worryJudge = 0
+    private var myPageJudge = 0
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setFragment()
+        setBottomBar()
+    }
+
+    private fun setFragment() {
+        val fragmentManager = supportFragmentManager
+        fragmentManager.beginTransaction().replace(R.id.contentFrame, counselFragment).commit()
+
+        fragmentManager.beginTransaction().add(R.id.contentFrame, myPageFragment).commit()
+        fragmentManager.beginTransaction().hide(myPageFragment).commit()
+
+        fragmentManager.beginTransaction().add(R.id.contentFrame, worryFragment).commit()
+        fragmentManager.beginTransaction().hide(worryFragment).commit()
+    }
+
+    private fun setBottomBar() {
+
+        counselTapView.setOnClickListener {
+            if(counselJudge == 0) {
+                counselJudge = 1
+                worryJudge = 0
+                myPageJudge = 0
+                counselTapView.setImageResource(R.drawable.ic_checked)
+                worryTapView.setImageResource(R.drawable.ic_unchecked)
+                myPageTapView.setImageResource(R.drawable.ic_unchecked)
+                switchFragment(FRAG_COUNSEL)
+            }
+        }
+
+        worryTapView.setOnClickListener {
+            if(worryJudge == 0) {
+                counselJudge = 0
+                worryJudge = 1
+                myPageJudge = 0
+                counselTapView.setImageResource(R.drawable.ic_unchecked)
+                worryTapView.setImageResource(R.drawable.ic_checked)
+                myPageTapView.setImageResource(R.drawable.ic_unchecked)
+                switchFragment(FRAG_WORRY)
+            }
+        }
+
+        addTapView.setOnClickListener {
+            startActivityForResult(Intent(this@MainActivity, AddWorryActivity::class.java), REQUEST_MAINACTIVITY_CODE)
+        }
+
+        myPageTapView.setOnClickListener {
+            if(myPageJudge == 0) {
+                counselJudge = 0
+                worryJudge = 0
+                myPageJudge = 1
+                counselTapView.setImageResource(R.drawable.ic_unchecked)
+                worryTapView.setImageResource(R.drawable.ic_unchecked)
+                myPageTapView.setImageResource(R.drawable.ic_checked)
+                switchFragment(FRAG_MYPAGE)
+            }
+        }
+    }
+
+    private fun switchFragment(next: Int) {
+        val fragmentManager = supportFragmentManager
+        when (next) {
+            FRAG_COUNSEL -> {
+                fragmentManager.beginTransaction().show(counselFragment).commit()
+                fragmentManager.beginTransaction().hide(worryFragment).commit()
+                fragmentManager.beginTransaction().hide(myPageFragment).commit()
+            }
+            FRAG_WORRY -> {
+                fragmentManager.beginTransaction().hide(counselFragment).commit()
+                fragmentManager.beginTransaction().show(worryFragment).commit()
+                fragmentManager.beginTransaction().hide(myPageFragment).commit()
+            }
+            FRAG_MYPAGE -> {
+                fragmentManager.beginTransaction().hide(counselFragment).commit()
+                fragmentManager.beginTransaction().hide(worryFragment).commit()
+                fragmentManager.beginTransaction().show(myPageFragment).commit()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == REQUEST_MAINACTIVITY_CODE) {
+            if(resultCode == RETURN_OK) {
+                counselJudge = 0
+                worryJudge = 1
+                myPageJudge = 0
+                counselTapView.setImageResource(R.drawable.ic_unchecked)
+                worryTapView.setImageResource(R.drawable.ic_checked)
+                myPageTapView.setImageResource(R.drawable.ic_unchecked)
+                switchFragment(FRAG_WORRY)
+            }
+        }
     }
 }

84febbcdf0b6de3e44e68ce8a5bd6faf4d3bdcd1
inju2403 <inju2403@gmail.com>
2020-08-12 20:43:08 +0900

    add CounselActivity

--- app/src/main/java/com/example/dayworry/counsel/CounselFragment.kt
+++ app/src/main/java/com/example/dayworry/counsel/CounselFragment.kt
@@ -1,11 +1,13 @@
 package com.example.dayworry.counsel
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.dayworry.R
+import kotlinx.android.synthetic.main.fragment_counsel.*
 
 class CounselFragment : Fragment() {
 
@@ -17,4 +19,11 @@ class CounselFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_counsel, container, false)
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        counselFragmentConstraintLayout.setOnClickListener{
+            startActivity(Intent(context, CounselActivity::class.java))
+        }
+    }
 }

acceefad696e579af5d7ce4d6db81692eb8e7e07
inju2403 <inju2403@gmail.com>
2020-08-23 21:27:23 +0900

    1. add ViewPager2 to CounselListFragment.kt 2. add DotsIndicator to CounselListFragment.kt

--- app/src/main/java/com/example/dayworry/counsel/CounselListFragment.kt
+++ app/src/main/java/com/example/dayworry/counsel/CounselListFragment.kt
@@ -1,15 +1,27 @@
 package com.example.dayworry.counsel
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 import com.example.dayworry.R
+import com.example.dayworry.counsel.counselList.CounselFragment1
+import com.example.dayworry.counsel.counselList.CounselFragment2
+import com.example.dayworry.counsel.counselList.CounselFragment3
+import com.example.dayworry.counsel.counselList.viewPager.DepthPageTransformer
+import com.example.dayworry.counsel.counselList.viewPager.ZoomOutPageTransformer
+import com.example.dayworry.utils.Constants.NUM_PAGES
 import kotlinx.android.synthetic.main.fragment_counsel_list.*
 
+@RequiresApi(21)
 class CounselListFragment : Fragment() {
+
+    private lateinit var viewPager: ViewPager2
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,8 +34,35 @@ class CounselListFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        counselFragmentConstraintLayout.setOnClickListener{
-            startActivity(Intent(context, CounselDetailActivity::class.java))
+        // Instantiate a ViewPager2 and a PagerAdapter.
+        viewPager = activity!!.findViewById(R.id.counsel_list_view_pager)
+
+        // The pager adapter, which provides the pages to the view pager widget.
+        val pagerAdapter = ScreenSlidePagerAdapter(activity!!)
+        viewPager.adapter = pagerAdapter
+
+//        viewPager.setPageTransformer(ZoomOutPageTransformer())
+        viewPager.setPageTransformer(DepthPageTransformer())
+
+        counsel_list_view_pager.apply {
+            adapter = viewPager.adapter
+            orientation = ViewPager2.ORIENTATION_HORIZONTAL
+            dots_indicator.setViewPager2(viewPager)
+        }
+    }
+
+    private inner class ScreenSlidePagerAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
+        override fun getItemCount(): Int = NUM_PAGES
+
+//        override fun createFragment(position: Int): Fragment = CounselFragment1()
+        override fun createFragment(position: Int): Fragment {
+            lateinit var frag: Fragment
+            when(position) {
+                0 -> frag = CounselFragment1()
+                1 -> frag = CounselFragment2()
+                2 -> frag = CounselFragment3()
+            }
+            return frag
         }
     }
 }
--- app/src/main/java/com/example/dayworry/utils/Constants.kt
+++ app/src/main/java/com/example/dayworry/utils/Constants.kt
@@ -3,5 +3,9 @@ package com.example.dayworry.utils
 object Constants {
     const val API_BASE_URL = "http:/"
     const val TAG: String = "로그"
-    val PREFERENCE = "template.android.DayWorry"
+    const val PREFERENCE = "template.android.DayWorry"
+    const val NUM_PAGES = 3
+
+    const val MIN_SCALE = 0.85f
+    const val MIN_ALPHA = 0.5f
 }

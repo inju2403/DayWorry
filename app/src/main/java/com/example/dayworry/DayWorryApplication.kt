169392e52c24393a1d4d524b7b20368462627c1f
inju2403 <inju2403@gmail.com>
2020-08-24 21:03:20 +0900

    raise the sdk version from 15 to 19

--- app/src/main/java/com/example/dayworry/DayWorryApplication.kt
+++ app/src/main/java/com/example/dayworry/DayWorryApplication.kt
@@ -1,11 +1,15 @@
 package com.example.dayworry
 
 import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
 import io.realm.Realm
 
 class DayWorryApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         Realm.init(this)
+
+        // Kakao SDK 초기화
+        KakaoSdk.init(this, "{NATIVE_APP_KEY}")
     }
 }

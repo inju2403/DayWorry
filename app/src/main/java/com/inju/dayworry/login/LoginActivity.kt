a37d1364160c97f7c5d77dbb63717cbfd8bb159e
inju2403 <inju2403@gmail.com>
2020-09-14 23:28:48 +0900

    add LoginActivity to getHashKey method

--- app/src/main/java/com/inju/dayworry/login/LoginActivity.kt
+++ app/src/main/java/com/inju/dayworry/login/LoginActivity.kt
@@ -1,7 +1,12 @@
 package com.inju.dayworry.login
 
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Base64
+import android.util.Base64.NO_WRAP
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.inju.dayworry.MainActivity
@@ -11,12 +16,15 @@ import com.kakao.sdk.auth.LoginClient
 import com.kakao.sdk.auth.rx
 import com.kakao.sdk.user.UserApiClient
 import com.kakao.sdk.user.rx
+import com.kakao.util.helper.Utility.getPackageInfo
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.addTo
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_login.*
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
 
 
 class LoginActivity : AppCompatActivity() {
@@ -27,24 +35,28 @@ class LoginActivity : AppCompatActivity() {
 
         val disposables = CompositeDisposable()
 
+
         login_button.setOnClickListener {
 
+//            var keyHash = getHashKey(this)
+//            Log.d(TAG, keyHash)
+
             //임시 라우팅
-            startActivity(Intent(this, SetProfileActivity::class.java))
-            finish()
+//            startActivity(Intent(this, SetProfileActivity::class.java))
+//            finish()
 
             // 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
-//            Single.just(LoginClient.instance.isKakaoTalkLoginAvailable(this))
-//                .flatMap { available ->
-//                    if (available) LoginClient.rx.loginWithKakaoTalk(this)
-//                    else LoginClient.rx.loginWithKakaoAccount(this)
-//                }
-//                .observeOn(AndroidSchedulers.mainThread())
-//                .subscribe({ token ->
-//                    Log.i(TAG, "로그인 성공 ${token.accessToken}")
-//                }, { error ->
-//                    Log.e(TAG, "로그인 실패", error)
-//                })
+            Single.just(LoginClient.instance.isKakaoTalkLoginAvailable(this))
+                .flatMap { available ->
+                    if (available) LoginClient.rx.loginWithKakaoTalk(this)
+                    else LoginClient.rx.loginWithKakaoAccount(this)
+                }
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({ token ->
+                    Log.i(TAG, "로그인 성공 ${token.accessToken}")
+                }, { error ->
+                    Log.e(TAG, "로그인 실패", error)
+                })
 //                .addTo(disposables)
         }
 
@@ -81,5 +93,38 @@ class LoginActivity : AppCompatActivity() {
 //            })
 //            .addTo(disposables)
 
+    }
+
+    fun getHashKey(context: Context): String? {
+        try {
+            if (Build.VERSION.SDK_INT >= 28) {
+                val packageInfo = getPackageInfo(context, PackageManager.GET_SIGNING_CERTIFICATES)
+                val signatures = packageInfo.signingInfo.apkContentsSigners
+                val md = MessageDigest.getInstance("SHA")
+                for (signature in signatures) {
+                    md.update(signature.toByteArray())
+                    return String(Base64.encode(md.digest(), NO_WRAP))
+                }
+            } else {
+                val packageInfo =
+                    getPackageInfo(context, PackageManager.GET_SIGNATURES) ?: return null
+
+                for (signature in packageInfo!!.signatures) {
+                    try {
+                        val md = MessageDigest.getInstance("SHA")
+                        md.update(signature.toByteArray())
+                        return Base64.encodeToString(md.digest(), Base64.NO_WRAP)
+                    } catch (e: NoSuchAlgorithmException) {
+                        // ERROR LOG
+                    }
+                }
+            }
+        } catch (e: PackageManager.NameNotFoundException) {
+            e.printStackTrace()
+        } catch (e: NoSuchAlgorithmException) {
+            e.printStackTrace()
+        }
+
+        return null
     }
 }

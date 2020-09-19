20a906aa8f018eaad13f22df14e2970b218f6ef3
inju2403 <inju2403@gmail.com>
2020-09-20 00:05:57 +0900

    modify ApiService temporarily related to the Worry

--- app/src/main/java/com/inju/dayworry/retrofit/ApiService.kt
+++ app/src/main/java/com/inju/dayworry/retrofit/ApiService.kt
@@ -1,13 +1,25 @@
 package com.inju.dayworry.retrofit
 
+import com.inju.dayworry.model.pojo.ContentsPOJO
 import com.inju.dayworry.model.pojo.Worry
 import io.reactivex.Single
-import retrofit2.http.GET
-import retrofit2.http.Header
+import retrofit2.Response
+import retrofit2.http.*
 
 interface ApiService {
 
-    //고민 리스트 받기
-    @GET("path")
+    @GET("path/") //고민 리스트 받기
     fun getWorrys(@Header("Authorization") Authorization: String): Single<Worry>
+
+    @GET("path/{id}/") //고민 받기
+    fun getWorryById(@Path("id") id: Long, @Header("Authorization") Authorization: String): Single<Worry>
+
+    @PUT("path/{id}/") //고민 수정
+    fun updateWorry(@Path("id") id: Long,
+                    @Body contentsPOJO: ContentsPOJO,
+                    @Header("Authorization") Authorization: String): Response<Unit>
+
+    @DELETE("path/{id}/") //고민 삭제
+    fun deleteWorry(@Path("id") id: Long, @Header("Authorization") Authorization: String): Response<Unit>
+
 }

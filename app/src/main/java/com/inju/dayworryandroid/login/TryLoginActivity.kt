54c2d4681afdbb6b1f92c8aa1cafb7f6f18a692a
inju2403 <56947879+inju2403@users.noreply.github.com>
2020-12-15 23:49:10 +0900

    Add TryLoginActivity.kt

---
+++ app/src/main/java/com/inju/dayworryandroid/login/TryLoginActivity.kt
@@ -0,0 +1,12 @@
+package com.inju.dayworryandroid.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.inju.dayworryandroid.R
+
+class TryLoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_try_login)
+    }
+}
